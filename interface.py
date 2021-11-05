c854902aec957aa4937bedb6360956d1f360dcce
zrh <1448054649@qq.com>
2021-11-05 23:16:43 +0800

    基本完成，debug改错了

--- interface.py
+++ interface.py
@@ -1,45 +1,54 @@
+from concurrent.futures import ThreadPoolExecutor
+from random import random
+
 import requests,time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import math
 
-# 直播间链接（队列，循环）
-from main import pool
+pool = ThreadPoolExecutor(max_workers=5)
 
-def start_task(ads_id,comments,studio_url,studio_url_queue,is_dynamic,comment_num,comment_interval):
+def start_task(ads_id,comments,studio_url_queue,is_dynamic,comment_num,comment_interval,each_group_studio_num):
     # 操控浏览器
     open_url = "http://local.adspower.net:50325/api/v1/browser/start?user_id=" + ads_id
     close_url = "http://local.adspower.net:50325/api/v1/browser/stop?user_id=" + ads_id
-    #studio_url=studio_url_queue.get()
     resp = requests.get(open_url).json()
     chrome_driver = resp["data"]["webdriver"]
     chrome_options = Options()
     chrome_options.add_experimental_option("debuggerAddress", resp["data"]["ws"]["selenium"])
-    driver = webdriver.Chrome(chrome_driver, options=chrome_options)
-    print(driver.title)
-    if (is_dynamic):
-        #开启动态抓取
+    # 直播间链接（队列，循环）
+    for i in range(each_group_studio_num):
+        studio_url = studio_url_queue.get()
+        if (is_dynamic):
+            #开启动态抓取
+            studio_url_queue=get_url(studio_url, studio_url_queue)
+        current_comments_num = []  # list当前评论数量
+        #判断是否下播
+        driver = webdriver.Chrome(chrome_driver, options=chrome_options)
         driver.get(studio_url)
-    time.sleep(10)
-    current_comments_num = 0  # 当前评论数量
-    #抓取其他url
-    get_url(studio_url, studio_url_queue)
-    is_disabled = driver.find_element(By.CLASS_NAME, "webcast-chatroom__comment-wrap")
-    if (is_disabled.value_of_css_property('cursor') != 'not-allowed'):
-        print('go on')
-        comm = driver.find_element(By.CLASS_NAME, "public-DraftStyleDefault-ltr")
-        comm.send_keys(comments)
         time.sleep(10)
-        driver.find_element(By.CLASS_NAME, "webcast-chatroom__comment-post").click()
-        current_comments_num+=1 #发送成功一次
-        time.sleep(10)
-        # driver.quit()z
-        # requests.get(close_url)
-        return current_comments_num
+        is_disabled = driver.find_element(By.CLASS_NAME, "webcast-chatroom__comment-wrap")
+        #从list随机抽取评论
+        comments=random.sample(comments, comment_num)
+        #发送若干条评论
+        count=0
+        for i in range(comment_num):
+            if (is_disabled.value_of_css_property('cursor') != 'not-allowed'):
+                print('go on')
+                comm = driver.find_element(By.CLASS_NAME, "public-DraftStyleDefault-ltr")
+                comm.send_keys(comments[i])
+                driver.find_element(By.CLASS_NAME, "webcast-chatroom__comment-post").click()
+                count+=1 #发送成功一次
+                #等待后进行下一次发送
+                time.sleep(comment_interval)
+                # driver.quit()
+                # requests.get(close_url)
+        current_comments_num.append(count)
+    return current_comments_num
 
 def get_url(studio_url,studio_url_queue):
-    return 0
+
+    return studio_url_queue
 #返回一个二维列表
 def split_groups(ads_id,each_group_num):
     splited_groups=[]
@@ -47,13 +56,11 @@ def split_groups(ads_id,each_group_num):
         splited_groups.append(ads_id[i:i + each_group_num])
     return splited_groups
 
-def get_groups_num(total_num,each_group_num):
-    return math.ceil(total_num/each_group_num)
 
-def manage(group, comments, studio_url, studio_url_queue, is_dynamic,comment_num,comment_interval):
+def manage(group, comments, studio_url_queue, is_dynamic,comment_num,comment_interval):
     list=[]
     for ads_id in group:
-        task = pool.submit(start_task(), ads_id, comments, studio_url, studio_url_queue, is_dynamic,comment_num,comment_interval)
+        task = pool.submit(start_task(), ads_id, comments, studio_url_queue, is_dynamic,comment_num,comment_interval)
         list.append(task)
     for task in list:
         task.result()
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from concurrent.futures import ThreadPoolExecutor
 
-from interface import split_groups, get_groups_num, manage
+from interface import split_groups, manage
 import queue,time,math
 
 '''参数'''
-comments=["aaaa","bbbbb"] #话术（list）
-ads_id = ["i13ovhc","i13un9b" ] #浏览器id
-comment_interval=10 #评论间隔
-comment_num=10 #评论数量
-total_num=20 #总账号数
-each_group_num=5 #单组账号数
-similtaneous_groups_num=get_groups_num(total_num,each_group_num) #同时运行分组数
+comments=["hi1","hi2","hi3","hi4","hi5","hi6","hi7","hi8","hi9","hi10"] #话术（list）,随机
+#5个账号
+ads_id = ["i13ovhc","i13un9b","i13kun7","i141fib","i141fic"] #浏览器id
+comment_interval=5 #评论间隔
+comment_num=5 #评论数量
+total_num=len(ads_id) #总账号数
+each_group_num=3 #单组账号数
+similtaneous_groups_num=1 #同时运行分组数
 
 #直播间队列初始化
 studio_url_queue=queue.Queue()
@@ -23,28 +24,27 @@ for item in ['https://www.tiktok.com/@mysticalempress233/live?lang=en',
 
 studio_url_queue_num=studio_url_queue.qsize() #直播间队列内数量
 each_group_studio_num= math.floor(studio_url_queue_num/similtaneous_groups_num) #每组分配直播间数
-splited_groups=split_groups(total_num,each_group_num) #返回的分组嵌套列表
+splited_groups=split_groups(ads_id,each_group_num) #返回的分组嵌套列表
 groups_num=len(splited_groups) #组数
-loop=groups_num/similtaneous_groups_num #循环数
+loop=int(groups_num/similtaneous_groups_num) #循环数
 is_dynamic=False #动态抓取，默认否
 
 '''执行过程'''
 #线程池
-pool = ThreadPoolExecutor(max_workers=10)
+pool = ThreadPoolExecutor(max_workers=5)
 
 #循环次数
 for l in range(loop):
-    print('this is '+ l +'round')
+    print('this is '+ str(l) +'round')
     #分组执行,先取出同时运行的组
     similtaneous_groups=splited_groups[l:l+similtaneous_groups_num]
     count=0
     list=[]
     for group in similtaneous_groups:
         #每个组一个线程，然后管理线程，进入同一个直播间
-        print('group ' + count + ' start' + time.time())
+        print('group ' + str(count) + ' start' + str(time.time()))
         count+=1
-        studio_url = studio_url_queue.get()
-        task = pool.submit(manage,group, comments, studio_url, studio_url_queue, is_dynamic,comment_num,comment_interval)
+        task = pool.submit(manage,group, comments, studio_url_queue, is_dynamic,comment_num,comment_interval)
         list.append(task)
     for task in list:
         task.result()
